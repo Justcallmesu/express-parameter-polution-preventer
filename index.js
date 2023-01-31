28f2cc6806f9ea2936738fa57d78dad20b44c82c
codenoobies <kevindubidaam@gmail.com>
2023-01-31 17:20:34 +0700

    Updating Index.js File

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const ErrorClass = require(`${__dirname}/modules/class/ErrorHandler.js`);
 
 function parameterPolutionPrevent(options) {
     return (req, res, next) => {
+        const { join, whitelist } = options;
         const ObjectQuery = [];
         const keys = [];
 
-        Object.keys(req.query).map(key => {
-            if (Array.isArray(req.query[key])) {
-                req.query[key] = req.query[key].join(" ");
+        Object.keys(req.query).map(keys => {
+            if (!whitelist.includes(keys) && Array.isArray(req.query[keys])) {
+                if (Array.isArray(whitelist)) {
+                    req.query[keys] = join.includes(keys) ? req.query[keys].join(" ") : req.query[keys][0];
+                } else {
+                    req.query[keys] = join ? req.query[keys].join(" ") : req.query[keys][0];
+                }
             }
 
-            if (typeof req.query[key] !== "string" && typeof req.query[key] === "object") {
-                ObjectQuery.push({ ...req.query[key], origin: key });
-                keys.push(Object.keys(req.query[key]));
+            if (typeof req.query[keys] !== "string" && typeof req.query[keys] === "object") {
+                ObjectQuery.push({ ...req.query[keys], origin: keys });
+                keys.push(Object.keys(req.query[keys]));
             }
         });
 
